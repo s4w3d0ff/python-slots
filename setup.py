b8eebfb07983f06949ee87c7ad9c641a926bc8d7
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2017-03-16 16:10:07 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='slots',
+    version='0.0.1',
+    description='Simple, expandable, customizable slot machine',
+    url='https://github.com/s4w3d0ff/python-slots',
+    author='s4w3d0ff',
+    license='GPL v2',
+    packages=['slots'],
+    zip_safe=False)
