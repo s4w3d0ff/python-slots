e4ed347fac7af3b472b5160d64ea8852123a4165
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2018-01-15 11:23:35 -0800

    fix requirements and version bump

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 setup(name='slots',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple, expandable, customizable slot machine',
     url='https://github.com/s4w3d0ff/python-slots',
     author='s4w3d0ff',
     license='GPL v2',
     packages=['slots'],
+    install_requires=['tqdm', 'numpy'],
     zip_safe=False)
