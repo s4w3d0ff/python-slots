f6ea00e606c996ed8cebbf20aa36f28f54834794
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2019-05-28 19:54:53 -0700

    Testing autodeploy

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup
 setup(name='slots',
-    version='0.0.2',
+    version='0.0.3',
     description='Simple, expandable, customizable slot machine',
     url='https://github.com/s4w3d0ff/python-slots',
     author='s4w3d0ff',
     license='GPL v2',
     packages=['slots'],
-    install_requires=['tqdm', 'numpy'],
-    zip_safe=False)
+    install_requires=['tqdm'],
+    extras_require={'numpy': ['numpy']}
+    zip_safe=False,
+    keywords=['slots', 'slotmachine', 'slot-machine'],
+    classifiers = [
+          'Intended Audience :: Game Developers',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2.7+',
+          'Programming Language :: Python :: 3.4+'
+          ]
+    )
